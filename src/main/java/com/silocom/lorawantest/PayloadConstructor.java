5c9a02c7d89c39e02b3fd1a2c9d241f2769f2db9
Henry Varona <henry.varona@gmail.com>
2020-01-02 09:44:50 -0400

    se arego el decode de join accept

--- src/main/java/com/silocom/lorawantest/PayloadConstructor.java
+++ src/main/java/com/silocom/lorawantest/PayloadConstructor.java
@@ -3,9 +3,12 @@
  */
 package com.silocom.lorawantest;
 
-import com.silocom.m2m.layer.physical.Connection;
-import java.util.Arrays;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
 
@@ -70,23 +73,180 @@ public class PayloadConstructor {
         message[14] = mic[2];
         message[15] = mic[3];
 
-        byte[] buffer = new byte[17];
         try {
             SecretKeySpec key = new SecretKeySpec(appKey, "AES");
             Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
             cipher.init(Cipher.DECRYPT_MODE, key);
-            buffer = cipher.update(message, 0, 16);
+            byte[] buffer = cipher.update(message, 0, message.length);
+            message = new byte[buffer.length + 1];
+            System.arraycopy(buffer, 0, message, 1, buffer.length);
+            message[0] = mhdr;
+            
+            decodeJoinAccept(message, appKey);
+
+            payloadB64 = Base64.encodeBase64String(message);
+
+            return jsonCons.SendJson(payloadB64, imme, tmst, freq, rfch, powe, modu, datr, codr, ipol, message.length, ncrc);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        message = new byte[17];
-        System.arraycopy(buffer, 0, message, 1, 16);
-        message[0] = mhdr;
-
-        payloadB64 = Base64.encodeBase64String(message);
-
-        return jsonCons.SendJson(payloadB64, imme, tmst, freq, rfch, powe, modu, datr, codr, ipol, message.length, ncrc);
 
     }
 
+    public static JoinAcceptMessage decodeJoinAccept(byte[] message, byte[] appKey) {
+        try {
+            byte[] srcmsg = message;
+            int index = 0;
+            byte MHDR = srcmsg[index];
+            index++;
+
+            SecretKeySpec key = new SecretKeySpec(appKey, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] buffer = cipher.update(srcmsg, 1, srcmsg.length - 1);
+            srcmsg = new byte[17];
+            srcmsg[0] = MHDR;
+            System.arraycopy(buffer, 0, srcmsg, 1, buffer.length);
+
+            byte[] msgToMic = new byte[srcmsg.length - 4];
+            System.arraycopy(srcmsg, 0, msgToMic, 0, msgToMic.length);
+
+            byte[] AppNonce = new byte[3];
+
+            for (int i = 2; i >= 0; i--) {
+                AppNonce[i] = srcmsg[index];
+                index++;
+            }
+            byte[] NetID = new byte[3];
+            for (int i = 2; i >= 0; i--) {
+                NetID[i] = srcmsg[index];
+                index++;
+            }
+            byte[] DevAddr = new byte[4];
+            for (int i = 3; i >= 0; i--) {
+                DevAddr[i] = srcmsg[index];
+                index++;
+            }
+            byte DLSetting = srcmsg[index];
+            index++;
+            byte RxDelay = srcmsg[index];
+            index++;
+            System.out.println("srcmsg.length " + srcmsg.length + " " + index);
+            byte[] CFList = new byte[srcmsg.length - (index + 4)];
+            for (int i = CFList.length - 1; i >= 0; i--) {
+                CFList[i] = srcmsg[index];
+                index++;
+            }
+
+            byte[] mic = new byte[4];
+            System.arraycopy(srcmsg, srcmsg.length - 4, mic, 0, 4);
+            System.out.println("MHDR : " + Integer.toHexString(MHDR));
+            System.out.println("AppNonce : " + Utils.hexToString(AppNonce));
+            System.out.println("NetId : " + Utils.hexToString(NetID));
+            System.out.println("DevAddr : " + Utils.hexToString(DevAddr));
+            System.out.println("DLSetting : " + Integer.toHexString(DLSetting));
+            System.out.println("RxDelay : " + Integer.toHexString(RxDelay));
+            System.out.println("CFList : " + Utils.hexToString(CFList));
+            System.out.println("Mic : " + Utils.hexToString(mic));
+            return new JoinAcceptMessage(MHDR, AppNonce, NetID, DevAddr, DLSetting, RxDelay, mic);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(PayloadConstructor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchPaddingException ex) {
+            Logger.getLogger(PayloadConstructor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeyException ex) {
+            Logger.getLogger(PayloadConstructor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static class JoinAcceptMessage {
+
+        byte mhdr;
+        byte[] appNonce;
+        byte[] netId;
+        byte[] devAddr;
+        byte dlSetting;
+        byte rxDelay;
+        byte[] CFList = null;
+        byte[] mic;
+
+        public JoinAcceptMessage() {
+        }
+
+        public JoinAcceptMessage(byte mhdr, byte[] appNonce, byte[] netId, byte[] devAddr, byte dlSetting, byte rxDelay, byte[] mic) {
+            this.mhdr = mhdr;
+            this.appNonce = appNonce;
+            this.netId = netId;
+            this.devAddr = devAddr;
+            this.dlSetting = dlSetting;
+            this.rxDelay = rxDelay;
+            this.mic = mic;
+        }
+
+        public byte getMhdr() {
+            return mhdr;
+        }
+
+        public void setMhdr(byte mhdr) {
+            this.mhdr = mhdr;
+        }
+
+        public byte[] getAppNonce() {
+            return appNonce;
+        }
+
+        public void setAppNonce(byte[] appNonce) {
+            this.appNonce = appNonce;
+        }
+
+        public byte[] getNetId() {
+            return netId;
+        }
+
+        public void setNetId(byte[] netId) {
+            this.netId = netId;
+        }
+
+        public byte[] getDevAddr() {
+            return devAddr;
+        }
+
+        public void setDevAddr(byte[] devAddr) {
+            this.devAddr = devAddr;
+        }
+
+        public byte getDlSetting() {
+            return dlSetting;
+        }
+
+        public void setDlSetting(byte dlSetting) {
+            this.dlSetting = dlSetting;
+        }
+
+        public byte getRxDelay() {
+            return rxDelay;
+        }
+
+        public void setRxDelay(byte rxDelay) {
+            this.rxDelay = rxDelay;
+        }
+
+        public byte[] getCFList() {
+            return CFList;
+        }
+
+        public void setCFList(byte[] CFList) {
+            this.CFList = CFList;
+        }
+
+        public byte[] getMic() {
+            return mic;
+        }
+
+        public void setMic(byte[] mic) {
+            this.mic = mic;
+        }
+
+    }
 }
